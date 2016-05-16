66b5635ffe690c0df13f60e3bc514f64d1e30ac7
Asko Soukka <asko.soukka@iki.fi>
2016-05-16 16:26:31 +0300

    Add example view

--- src/plone.server/plone/server/browser.py
+++ src/plone.server/plone/server/browser.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
-from aiohttp.web import Response
+from aiohttp.web import json_response
 from plone.dexterity.interfaces import IDexterityContent
 from plone.registry.interfaces import IRegistry
 from plone.server.interfaces import IRequest
 from plone.server.interfaces import IView
 from zope.component import adapter
 from zope.interface import implementer
+
+
+def get_physical_path(context):
+    parts = [context.__name__]
+    parent = context.__parent__
+    while parent is not None and parent.__name__ is not None:
+        parts.append(parent.__name__)
+        parent = parent.__parent__
+    parts.append('')
+    return reversed(parts)
 
 
 @adapter(IDexterityContent, IRequest)
@@ -17,6 +27,7 @@ class View(object):
         self.registry = self.request.registry.getUtility(IRegistry)
 
     async def __call__(self):
-        import pdb  # noqa
-        pdb.set_trace()  # noqa
-        return Response(text=str(self.registry))
+        return json_response({
+            'context': str(self.context),
+            'path': '/'.join(get_physical_path(self.context))
+        })
