ef0408be134508d0471ec7af6180d1f6281679aa
Asko Soukka <asko.soukka@iki.fi>
2016-05-19 19:00:34 +0300

    Add websocket service example

--- src/plone.server/plone/server/testing.py
+++ src/plone.server/plone/server/testing.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-import zope.component
 from zope.component import testlayer
+
 import plone.server
+import zope.component
 
 
 ZCMLLayer = testlayer.ZCMLFileLayer(plone.server, 'configure.zcml')
