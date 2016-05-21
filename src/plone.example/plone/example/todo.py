ce4f028207cff1ff0bb03b72c64e2e1a06f71e59
esteele <eric@esteele.net>
2016-05-21 19:07:46 -0400

    Make the Todo schema a bit more complex
    Adds
    * Field from a parent class
    * Fieldsets
    * Basic dublin core behavior

--- src/plone.example/plone/example/todo.py
+++ src/plone.example/plone/example/todo.py
@@ -10,30 +10,45 @@ from zope.component import adapter
 from zope.dublincore.annotatableadapter import ZDCAnnotatableAdapter
 from zope.dublincore.interfaces import IWriteZopeDublinCore
 from zope.interface import provider
+from zope.interface import implementer
 
 
-class ITodo(model.Schema):
+class IExampleBase(model.Schema):
     title = schema.TextLine(
         title='Title',
         required=False,
         description=u"Describe the task.",
         default=u''
     )
+
+
+class ITodo(IExampleBase):
     done = schema.Bool(
         title='Done',
         required=False,
         description=u'Has the task been completed?',
         default=False
     )
+    assigned_to = schema.TextLine(
+        title='Assigned To',
+        required=False,
+        description=u"The person who needs to complete the task.",
+        default=u''
+    )
     read_permission(notes='plone.example.classified')
     notes = schema.Text(
         title='Notes',
         required=False,
-        description=u'Classified notes on about task',
+        description=u'Classified notes about the task',
         default=u''
     )
 
+    model.fieldset(u'other',
+                   label=u'Additional Information',
+                   fields=['assigned_to', 'notes'])
 
+
+@implementer(ITodo)
 class Todo(Item):
     pass
 
@@ -51,9 +66,22 @@ class View(Service):
 
 
 @provider(IFormFieldProvider)
-class IDublinCore(IWriteZopeDublinCore):
+class IDublinCore(model.Schema):
     """We basically just want the IFormFieldProvider interface applied
         There's probably a zcml way of doing this. """
+    created = schema.Datetime(
+        title = u'Creation Date',
+        description =
+        u"The date and time that an object is created. "
+        u"\nThis is normally set automatically."
+        )
+
+    modified = schema.Datetime(
+        title = u'Modification Date',
+        description =
+        u"The date and time that the object was last modified in a\n"
+        u"meaningful way."
+        )
 
 
 @adapter(IDexterityContent)
