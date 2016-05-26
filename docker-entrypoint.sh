e66877d4aa2db45847c0c6ff1bc739d586d51140
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-05-26 21:35:16 +0200

    Better docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,52 @@
 #!/bin/bash
 set -e
 
-if [ -z "$OAUTH_HOST" ]; then
+if [ -n "$OAUTH_PORT_6543_TCP_ADDR" ]; then
+    if [ -z "$OAUTH_HOST" ]; then
+        OAUTH_HOST=$OAUTH_PORT_6543_TCP_ADDR
+    else
+        echo >&2 'warning: both OAUTH_HOST and OAUTH_PORT_6543_TCP_ADDR found'
+        echo >&2 "  Connecting to OAUTH_HOST ($OAUTH_HOST)"
+        echo >&2 '  instead of the linked redis container'
+    fi
+else
     OAUTH_HOST='oauth'
 fi
 
-if [ -z "$OAUTH_PORT" ]; then
+if [ -n "$OAUTH_PORT_6543_TCP_PORT" ]; then
+    if [ -z "$OAUTH_PORT" ]; then
+        OAUTH_PORT=$OAUTH_PORT_6543_TCP_PORT
+    else
+        echo >&2 'warning: both OAUTH_PORT and OAUTH_PORT_6543_TCP_PORT found'
+        echo >&2 "  Connecting to OAUTH_PORT ($OAUTH_PORT)"
+        echo >&2 '  instead of the linked redis container'
+    fi
+else
     OAUTH_PORT='6543'
 fi
 
-if [ -z "$ELASTIC_HOST" ]; then
-    ELASTIC_HOST='elasticsearch'
+
+if [ -n "$ELASTICSEARCH_PORT_9200_TCP_ADDR" ]; then
+    if [ -z "$OAUTH_HOST" ]; then
+        ELASTIC_HOST=$ELASTICSEARCH_PORT_9200_TCP_ADDR
+    else
+        echo >&2 'warning: both ELASTIC_HOST and ELASTICSEARCH_PORT_9200_TCP_ADDR found'
+        echo >&2 "  Connecting to ELASTIC_HOST ($OAUTH_HOST)"
+        echo >&2 '  instead of the linked redis container'
+    fi
+else
+    ELASTIC_HOST='oauth'
 fi
 
-if [ -z "$ELASTIC_PORT" ]; then
+if [ -n "$ELASTICSEARCH_PORT_9200_TCP_PORT" ]; then
+    if [ -z "$ELASTIC_PORT" ]; then
+        ELASTIC_PORT=$ELASTICSEARCH_PORT_9200_TCP_PORT
+    else
+        echo >&2 'warning: both ELASTIC_PORT and ELASTICSEARCH_PORT_9200_TCP_PORT found'
+        echo >&2 "  Connecting to ELASTIC_PORT ($ELASTIC_PORT)"
+        echo >&2 '  instead of the linked redis container'
+    fi
+else
     ELASTIC_PORT='9200'
 fi
 
@@ -58,7 +91,13 @@ echo '{
 }' > /app/src/plone.server/plone/server/auth/oauth.json
 
 
-sed -i 's/localhost:9200/$ELASTIC_HOST:$ELASTIC_PORT/' /app/src/plone.server/plone/server/search/elasticsearch.json
+sed -i 's/localhost:9200/$ELASTIC_HOST:$ELASTIC_PORT/' /app/src/plone.server/plone/server/catalog/elasticsearch.json
+
+until nc -z $ELASTIC_HOST $ELASTIC_PORT;
+do
+  echo "Waiting for oauth"
+  sleep 1
+done
 
 until nc -z $OAUTH_HOST $OAUTH_PORT;
 do
