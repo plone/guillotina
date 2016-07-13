1601a8d9b3c4ef3de2269948057419c034a9000b
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-07-13 18:10:30 +0200

    PUT and PATCH verbs for dexterity copied from plone.restapi

--- src/plone.server/plone/server/api/dexterity.py
+++ src/plone.server/plone/server/api/dexterity.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 from plone.jsonserializer.interfaces import ISerializeToJson
+from plone.jsonserializer.interfaces import IDeserializeFromJson
 from plone.server.api.service import Service
 from plone.server.registry import ICors
 from plone.server.browser import get_physical_path
 from zope.component import getMultiAdapter
-from aiohttp.web_exceptions import HTTPNotFound
-from aiohttp.web_exceptions import HTTPUnauthorized
-from aiohttp.web_exceptions import HTTPFound
-from plone.server.browser import ResponseWithHeaders
+from plone.server.browser import Response
+from plone.server.browser import ErrorResponse
+from plone.server.browser import UnauthorizedResponse
+from plone.server.interfaces import IAbsoluteUrl
+from plone.server import _
 import fnmatch
+from zope.security import checkPermission
+from zope.security.interfaces import Unauthorized
+from plone.dexterity.utils import createContent
+from zope.component import queryMultiAdapter
+import traceback
+from datetime import datetime
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class DefaultGET(Service):
@@ -21,10 +33,71 @@ class DefaultGET(Service):
 
 class DefaultPOST(Service):
     async def __call__(self):
-        serializer = getMultiAdapter(
-            (self.context, self.request),
-            ISerializeToJson)
-        return serializer()
+        """To create a content. Its a copy of plone.restapi"""
+        data = await self.request.json()
+        type_ = data.get('@type', None)
+        id_ = data.get('id', None)
+        title = data.get('title', None)
+
+        if not type_:
+            return ErrorResponse(
+                'RequiredParam',
+                _("Property '@type' is required"))
+
+        # Generate a temporary id if the id is not given
+        if not id_:
+            now = datetime.now()
+            new_id = '{}.{}.{}{:04d}'.format(
+                type_.lower().replace(' ', '_'),
+                now.strftime('%Y-%m-%d'),
+                str(now.millis())[7:],
+                randint(0, 9999))
+        else:
+            new_id = id_
+
+        # It already exists
+        if new_id in self.context:
+            return ErrorResponse(
+                'Conflict',
+                _("Id already exists"))
+
+        # Create object
+        try:
+            obj = createContent(type_, id=new_id, title=title)
+            self.context[new_id] = obj
+        except ValueError as e:
+            return ErrorResponse(
+                'DeserializationError',
+                str(e),
+                status=400)
+
+        # Update fields
+        deserializer = queryMultiAdapter((obj, self.request),
+                                         IDeserializeFromJson)
+        if deserializer is None:
+            return ErrorResponse(
+                'DeserializationError',
+                'Cannot deserialize type {}'.format(obj.portal_type),
+                status=501)
+
+        try:
+            deserializer(data, validate_all=True)
+        except DeserializationError as e:
+            return ErrorResponse(
+                'DeserializationError',
+                str(e),
+                status=400)
+
+        # Rename if generated id
+        if not id_:
+            self.rename_object(obj)
+
+        absolute_url = queryMultiAdapter((obj, self.request), IAbsoluteUrl)
+
+        headers = {
+            'Location': await absolute_url()
+        }
+        return Response(response={}, headers=headers, status=201)
 
 
 class DefaultPUT(Service):
@@ -32,7 +105,24 @@ class DefaultPUT(Service):
 
 
 class DefaultPATCH(Service):
-    pass
+    async def __call__(self):
+        deserializer = queryMultiAdapter((self.context, self.request),
+                                         IDeserializeFromJson)
+        if deserializer is None:
+            return ErrorResponse(
+                'DeserializationError',
+                'Cannot deserialize type {}'.format(obj.portal_type),
+                status=501)
+
+        try:
+            deserializer()
+        except DeserializationError as e:
+            return ErrorResponse(
+                'DeserializationError',
+                str(e),
+                status=400)
+
+        return Response(response={}, status=204)
 
 
 class SharingPOST(Service):
@@ -40,7 +130,10 @@ class SharingPOST(Service):
 
 
 class DefaultDELETE(Service):
-    pass
+
+    async def __call__(self):
+        content_id = self.context.id
+        del self.context.__parent__[content_id]
 
 
 class DefaultOPTIONS(Service):
@@ -76,7 +169,8 @@ class DefaultOPTIONS(Service):
         requested_method = requested_method.upper()
         allowed_methods = self.settings.allow_methods
         if requested_method not in allowed_methods:
-            raise HTTPNotFound(
+            raise HTTPMethodNotAllowed(
+                requested_method, allowed_methods,
                 text='Access-Control-Request-Method Method not allowed')
 
         supported_headers = self.settings.allow_headers
