c4c2bbb58545eb0f880ac9a05bfd17998060a663
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-07-13 18:08:41 +0200

    Absolute URL Adapter and Error Response classes to render them as son

--- src/plone.server/plone/server/browser.py
+++ src/plone.server/plone/server/browser.py
@@ -2,6 +2,7 @@
 from plone.dexterity.interfaces import IDexterityContent
 from plone.server.interfaces import IRequest
 from plone.server.interfaces import IView
+from plone.server.interfaces import IAbsoluteUrl
 from zope.component import adapter
 from zope.interface import implementer
 from zope.location import ILocation
@@ -38,8 +39,48 @@ class View(object):
         }
 
 
-class ResponseWithHeaders(object):
+@adapter(IDexterityContent, IRequest)
+@implementer(IAbsoluteUrl)
+class Absolute_URL(object):
 
-    def __init__(self, response, headers):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    async def __call__(self):
+        path = '/'.join(get_physical_path(self.context))
+        return self.request.scheme + '://' + self.request.host + '/' +\
+            self.request._db_id + path
+
+
+class Response(object):
+    """Middle response to be rendered."""
+
+    def __init__(self, response={}, headers={}, status=200):
         self.response = response
         self.headers = headers
+        self.status = status
+
+
+class UnauthorizedResponse(Response):
+
+    def __init__(self, message, headers={}, status=401):
+        response = {
+            'error': {
+                'type': 'Unauthorized',
+                'message': message
+            }
+        }
+        super(UnauthorizedResponse, self).__init__(response, headers, status)
+
+
+class ErrorResponse(Response):
+
+    def __init__(self, type, message, headers={}, status=400):
+        response = {
+            'error': {
+                'type': type,
+                'message': message
+            }
+        }
+        super(ErrorResponse, self).__init__(response, headers, status)
