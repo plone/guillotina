a14e5e60116a94bf34fb1760a5445637555150c6
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-11-20 13:42:20 +0100

    Missing patch file

---
+++ src/plone.server/plone/server/patch.py
@@ -0,0 +1,48 @@
+from zope.interface.adapter import BaseAdapterRegistry
+from zope.interface.adapter import AdapterLookupBase
+from zope.interface import providedBy
+
+import asyncio
+
+BaseAdapterRegistry._delegated = (
+    'lookup', 'queryMultiAdapter', 'lookup1', 'queryAdapter',
+    'adapter_hook', 'lookupAll', 'names',
+    'subscriptions', 'subscribers', 'asubscribers')
+
+
+async def asubscribers(self, objects, provided):
+    subscriptions = self.subscriptions(map(providedBy, objects), provided)
+    if provided is None:
+        result = ()
+        for subscription in subscriptions:
+            if asyncio.iscoroutinefunction(subscription):
+                await subscription(*objects)
+    else:
+        result = []
+        for subscription in subscriptions:
+            if asyncio.iscoroutinefunction(subscription):
+                subscriber = await subscription(*objects)
+            if subscriber is not None:
+                result.append(subscriber)
+    return result
+
+
+def subscribers(self, objects, provided):
+    subscriptions = self.subscriptions(map(providedBy, objects), provided)
+    if provided is None:
+        result = ()
+        for subscription in subscriptions:
+            if not asyncio.iscoroutinefunction(subscription):
+                subscription(*objects)
+    else:
+        result = []
+        for subscription in subscriptions:
+            if not asyncio.iscoroutinefunction(subscription):
+                subscriber = subscription(*objects)
+            if subscriber is not None:
+                result.append(subscriber)
+    return result
+
+AdapterLookupBase.asubscribers = asubscribers
+AdapterLookupBase.subscribers = subscribers
+
