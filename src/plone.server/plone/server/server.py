ffdc4fcb7e0f082150eec2dae6c11277f7c5cf3c
Ramon Navarro Bosch <ramon@atlasense.com>
2016-12-26 20:04:18 +0100

    Adding an option to pserver to add debug messages

--- src/plone.server/plone/server/server.py
+++ src/plone.server/plone/server/server.py
@@ -5,7 +5,7 @@ from plone.server.factory import make_app
 
 import argparse
 import json
-# import logging
+import logging
 
 
 # logger = logging.getLogger('plone.server')
@@ -18,10 +18,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--configuration',
                         default='config.json', help='Configuration file')
+    parser.add_argument('--debug', dest='debug', action='store_true',
+                        help='Log verbose')
+    parser.set_defaults(debug=False)
     arguments = parser.parse_args()
 
     with open(arguments.configuration, 'r') as config:
         settings = json.load(config)
+
+    if arguments.debug:
+        logging.basicConfig(
+            level=logging.DEBUG)
+
     web.run_app(make_app(
         config_file=arguments.configuration), port=settings['address'])
 
