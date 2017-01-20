8619af7185d2b8ea33c5e1aa8e0eeb33343bb5ae
vangheem <vangheem@gmail.com>
2017-01-20 03:56:28 -0600

    make sure json executes code so configurations are loaded

--- src/plone.server/plone/server/json/__init__.py
+++ src/plone.server/plone/server/json/__init__.py
@@ -1,1 +1,9 @@
-#
+# make sure configure registrations are executed
+from . import deserialize_content_fields  # noqa
+from . import deserialize_content  # noqa
+from . import deserialize_value  # noqa
+from . import serialize_content_field  # noqa
+from . import serialize_content  # noqa
+from . import serialize_schema_field  # noqa
+from . import serialize_schema  # noqa
+from . import serialize_value  # noqa
