b5e21c492ed1e6f779571aa5ca9eb5e566a5afdb
vangheem <vangheem@gmail.com>
2017-03-09 13:27:32 +0100

    remove layer module in api

--- guillotina/api/layer.py
+++
@@ -1,7 +0,0 @@
-from guillotina import logger
-from guillotina.interfaces import IDefaultLayer  # noqa
-
-
-logger.warn('guillotina.api.layer.IDefaultLayer has been moved to '
-            'guillotina.interfaces.IDefaultLayer. This import will '
-            'no longer work in version 2.0.0 of guillotina')
