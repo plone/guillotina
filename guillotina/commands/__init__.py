25217ef7f229cb12ae5360dfb7cc4f090da7680c
vangheem <vangheem@gmail.com>
2017-03-21 09:39:07 -0500

    Fix `gshell` command to work with aysncio loop so you can run `await` statements   with the shell. Compatibility done with aioconsole.

--- guillotina/commands/__init__.py
+++ guillotina/commands/__init__.py
@@ -42,45 +42,63 @@ class Command(object):
     description = ''
 
     def __init__(self):
+        '''
+        Split out into parts that can be overridden
+        '''
+        self.setup_fake_request()
+        self.parse_arguments()
+
+        settings = self.get_settings()
+        app = self.make_app(settings)
+
+        self.setup_logging()
+        self.run_command(app, settings)
+
+    def parse_arguments(self):
+        parser = self.get_parser()
+        self.arguments = parser.parse_args()
+
+    def run_command(self, app, settings):
+        if asyncio.iscoroutinefunction(self.run):
+            loop = asyncio.get_event_loop()
+            # Blocking call which returns when finished
+            loop.run_until_complete(self.run(self.arguments, settings, app))
+            loop.close()
+        else:
+            self.run(self.arguments, settings, app)
+
+    def setup_fake_request(self):
         self.request = get_mocked_request()
         login(self.request)
 
-        parser = self.get_parser()
-        arguments = parser.parse_args()
-
-        if os.path.exists(arguments.configuration):
-            with open(arguments.configuration, 'r') as config:
+    def get_settings(self):
+        if os.path.exists(self.arguments.configuration):
+            with open(self.arguments.configuration, 'r') as config:
                 settings = json.load(config)
         else:
             logger.warn('Could not find the configuration file {}. Using default settings.'.format(
-                arguments.configuration
+                self.arguments.configuration
             ))
             settings = MISSING_SETTINGS.copy()
+        return settings
 
-        app = self.make_app(settings)
-
+    def setup_logging(self):
         logging.basicConfig(stream=sys.stdout)
         logger.setLevel(logging.INFO)
         ch = logging.StreamHandler(sys.stdout)
         ch.setLevel(logging.INFO)
-
-        if arguments.debug:
+        if self.arguments.debug:
             logger.setLevel(logging.DEBUG)
             logging.basicConfig(
                 stream=sys.stdout,
                 level=logging.DEBUG)
             ch.setLevel(logging.DEBUG)
 
-        if asyncio.iscoroutinefunction(self.run):
-            loop = asyncio.get_event_loop()
-            # Blocking call which returns when finished
-            loop.run_until_complete(self.run(arguments, settings, app))
-            loop.close()
-        else:
-            self.run(arguments, settings, app)
+    def get_loop(self):
+        return asyncio.get_event_loop()
 
     def make_app(self, settings):
-        return make_app(settings=settings)
+        return make_app(settings=settings, loop=self.get_loop())
 
     def get_parser(self):
         parser = argparse.ArgumentParser(description=self.description)
--- guillotina/commands/shell.py
+++ guillotina/commands/shell.py
@@ -1,18 +1,64 @@
-from code import interact
 from guillotina import app_settings
 from guillotina.commands import Command
 from guillotina.component import getUtility
 from guillotina.interfaces import IApplication
 from guillotina.testing import TESTING_SETTINGS
+import aioconsole
+import argparse
+import asyncio
+
+
+class Console(aioconsole.code.AsynchronousConsole):
+    async def interact(self, banner=None, stop=True, handle_sigint=True):
+        """
+        We are now running in loop so we can do async stuff with guillotina
+        app and database...
+        """
+        root = self.locals['root']
+        tm = root['db'].get_transaction_manager()
+        await tm.begin()
+        self.locals['tm'] = tm
+        return await super().interact(banner, stop, handle_sigint)
+
+
+class InteractiveEventLoop(asyncio.SelectorEventLoop):
+    """Event loop running a python console."""
+
+    console_class = Console
+
+    def __init__(self, banner=''):
+        self.banner = banner
+        self.console = None
+        self.console_task = None
+        self.console_server = None
+        super().__init__(selector=None)
+
+    def setup(self, app):
+        '''
+        need to manually run this after app is initialized and we have
+        locals that matter to us...
+        '''
+        app_settings['root_user']['password'] = TESTING_SETTINGS['root_user']['password']
+        root = getUtility(IApplication, name='root')
+        _locals = {
+            'app': app,
+            'root': root,
+            'app_settings': app_settings
+        }
+        self.console = self.console_class(None, locals=_locals, loop=self)
+        coro = self.console.interact(self.banner, stop=True, handle_sigint=True)
+        self.console_task = asyncio.async(coro, loop=self)
+
+    def close(self):
+        if self.console_task and not self.is_running():
+            asyncio.Future.cancel(self.console_task)
+        super().close()
 
 
 class ShellCommand(Command):
     description = 'Guillotina server shell'
-
-    def run(self, arguments, settings, app):
-        app_settings['root_user']['password'] = TESTING_SETTINGS['root_user']['password']
-        root = getUtility(IApplication, name='root')
-        interact('''
+    loop = None
+    banner = '''
 guillotina interactive shell
 ==============================
 
@@ -21,9 +67,27 @@ Available local variables:
     - app
     - root
     - app_settings
+    - tm
+    - asyncio
+    - loop
 
-    ''', local={
-            'app': app,
-            'root': root,
-            'app_settings': app_settings
-        })
+Example
+-------
+
+site = await root['db'].async_get('site')
+
+'''
+
+    def get_loop(self):
+        if self.loop is None:
+            self.loop = InteractiveEventLoop(self.banner)
+            asyncio.set_event_loop(self.loop)
+        return self.loop
+
+    def run_command(self, app, settings):
+        loop = self.get_loop()
+        loop.setup(app)
+        try:
+            loop.run_forever()
+        except KeyboardInterrupt:
+            pass
