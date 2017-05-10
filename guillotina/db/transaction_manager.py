21e7d4df762fb0a3124924460010990bcb718c3e
vangheem <vangheem@gmail.com>
2017-05-09 22:07:46 -0500

    extra check

--- guillotina/db/transaction_manager.py
+++ guillotina/db/transaction_manager.py
@@ -47,18 +47,19 @@ class TransactionManager(object):
 
         txn = None
         if hasattr(request, '_txn'):
-            # already has txn registered, close connection on it...
+            # already has txn registered, as long as connection is closed, it
+            # is safe
             if getattr(request, '_db_conn', None) is None:
                 # re-use txn if possible
                 txn = request._txn
-            # XXX do we want to auto clean up here? This will break tests
-            # that are starting multiple transactions
+            # XXX do we want to auto clean up here? Or throw an error?
+            # This will break tests that are starting multiple transactions
             # else:
             #     await self._close_txn(request._txn)
         else:
             txn = Transaction(self, request=request)
 
-        self._last_txn = txn = Transaction(self, request=request)
+        self._last_txn = txn
 
         if request is not None:
             # register tm and txn with request
@@ -97,7 +98,7 @@ class TransactionManager(object):
             await self._close_txn(txn)
 
     async def _close_txn(self, txn):
-        if txn is not None:
+        if txn is not None and txn._db_conn is not None:
             await self._storage.close(txn._db_conn)
             txn._db_conn = None
         if txn == self._last_txn:
