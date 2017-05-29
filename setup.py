8b333c773a68c19a00918e08977c2b49a3de7060
vangheem <vangheem@gmail.com>
2017-05-29 08:02:44 -0500

    do not use latest aiohttp yet

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     packages=find_packages(),
     ext_modules=ext_modules,
     install_requires=[
-        'aiohttp>=2.0.0<3.0.0',
+        'aiohttp>=2.0.0<2.1.0',
         'jsonschema',
         'python-dateutil',
         'pycrypto',
