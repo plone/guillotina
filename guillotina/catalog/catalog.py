e44a77899528fa9689b9d581c475613b38588f11
vangheem <vangheem@gmail.com>
2017-09-22 17:38:26 -0500

    only load behavior if necessary

--- guillotina/catalog/catalog.py
+++ guillotina/catalog/catalog.py
@@ -125,24 +125,33 @@ class DefaultCatalogDataAdapter(object):
         value = getattr(ob, field_name, None)
         return json_compatible(value)
 
+    async def load_behavior(self, behavior):
+        if IAsyncBehavior.implementedBy(behavior.__class__):
+            # providedBy not working here?
+            await behavior.load(create=False)
+
     async def __call__(self, indexes=None):
         # For each type
         values = {}
 
         for schema in iter_schemata(self.content):
             behavior = schema(self.content)
-            if IAsyncBehavior.implementedBy(behavior.__class__):
-                # providedBy not working here?
-                await behavior.load(create=False)
+            loaded = False
             for index_name, index_data in merged_tagged_value_dict(schema, index.key).items():
                 try:
                     # accessors we always reindex since we can't know if updated
                     # from the indexes param--they are "fake" like indexes, not fields
                     if ('accessor' in index_data and (
                             indexes is None or index_data.get('field') in indexes)):
+                        if not loaded:
+                            await self.load_behavior(behavior)
+                            loaded = True
                         values[index_name] = index_data['accessor'](behavior)
                     elif (indexes is None or index_name in indexes or
                             isinstance(getattr(type(self.content), index_name, None), property)):
+                        if not loaded:
+                            await self.load_behavior(behavior)
+                            loaded = True
                         # in this case, properties are also dynamic so we have to make sure
                         # to allow for them to be reindexed every time.
                         values[index_name] = self.get_data(behavior, schema, index_name)
@@ -154,6 +163,9 @@ class DefaultCatalogDataAdapter(object):
                     # in this case, properties are also dynamic so we have to make sure
                     # to allow for them to be reindexed every time.
                     continue  # skip
+                if not loaded:
+                    await self.load_behavior(behavior)
+                    loaded = True
                 values[metadata_name] = self.get_data(behavior, schema, metadata_name)
 
         return values
