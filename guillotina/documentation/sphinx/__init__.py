4d59a8c12267ac26444c69a55be5e5de738f16a8
vangheem <vangheem@gmail.com>
2017-10-07 19:19:35 -0500

    try a zip file...

--- guillotina/documentation/sphinx/__init__.py
+++ guillotina/documentation/sphinx/__init__.py
@@ -135,13 +135,9 @@ class HTTPService(CodeBlock):
 
 
 def setup(app):
-    try:
-        app.add_html_theme(
-            'guillotina', os.path.join(
-                os.path.abspath(os.path.dirname(__file__)), 'theme'))
-    except AttributeError:
-        # not valid in sphinx 1.6
-        pass
+    app.add_html_theme(
+        'guillotina', os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), 'theme.zip'))
 
     app.add_directive_to_domain('http', 'service', HTTPService)
     dist = pkg_resources.get_distribution('guillotina')
