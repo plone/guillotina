341b8d728e9a53cf3ae37c70dbbf5841187bcc9b
vangheem <vangheem@gmail.com>
2017-12-16 17:31:06 -0500

    fix interface, it is an adapter

--- guillotina/constraintypes.py
+++ guillotina/constraintypes.py
@@ -1,14 +1,14 @@
 from guillotina import configure
-from guillotina.interfaces import IDatabase
 from guillotina.content import get_cached_factory
 from guillotina.interfaces import IConstrainTypes
+from guillotina.interfaces import IDatabase
 from zope.interface import Interface
 
 
 @configure.adapter(
     for_=Interface,
     provides=IConstrainTypes)
-class FTIConstrainAllowedTypes(object):
+class FTIConstrainAllowedTypes:
 
     def __init__(self, context: Interface):
         self.context = context
@@ -37,6 +37,9 @@ class DatabaseAllowedTypes:
     '''
     Can only add containers to databases
     '''
+
+    def __init__(self, context: Interface):
+        self.context = context
 
     def is_type_allowed(self, type_id: str) -> bool:
         return type_id == 'Container'
