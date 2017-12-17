d38ac7431908531ae099011f38cf1890cc425175
vangheem <vangheem@gmail.com>
2017-12-17 08:54:11 -0500

    a couple for tus

--- guillotina/tests/test_serialize.py
+++ guillotina/tests/test_serialize.py
@@ -6,12 +6,12 @@ from guillotina.files.dbfile import DBFile
 from guillotina.interfaces import IJSONToValue
 from guillotina.interfaces import IResourceDeserializeFromJson
 from guillotina.interfaces import IResourceSerializeToJson
+from guillotina.json import deserialize_value
 from guillotina.json.deserialize_value import schema_compatible
 from guillotina.json.serialize_value import json_compatible
 from guillotina.tests.utils import create_content
 from guillotina.tests.utils import login
 from zope.interface import Interface
-from guillotina.json import deserialize_value
 
 
 async def test_serialize_resource(dummy_request):
