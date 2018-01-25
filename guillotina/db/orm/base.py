b37aad1c4c8fa3a71396651aed5b6a7d9df91d8a
Nathan Van Gheem <vangheem@gmail.com>
2018-01-24 22:19:23 -0800

    Use __slots__ for BaseObject (#220)

--- guillotina/db/orm/base.py
+++ guillotina/db/orm/base.py
@@ -10,13 +10,13 @@ class ObjectProperty(object):
         self.default = default
 
     def __get__(self, inst, klass):
-        return getattr(inst, self.attribute, self.default)
+        return object.__getattribute__(inst, self.attribute)
 
     def __set__(self, inst, value):
-        setattr(inst, self.attribute, value)
+        object.__setattr__(inst, self.attribute, value)
 
     def __delete__(self, inst):
-        setattr(inst, self.attribute, self.default)
+        object.__setattr__(inst, self.attribute, self.default)
 
 
 class DictDefaultProperty(ObjectProperty):
@@ -24,14 +24,14 @@ class DictDefaultProperty(ObjectProperty):
         self.attribute = attribute
 
     def __get__(self, inst, klass):
-        val = getattr(inst, self.attribute, None)
+        val = object.__getattribute__(inst, self.attribute)
         if val is None:
-            setattr(inst, self.attribute, {})
-            return getattr(inst, self.attribute, None)
+            object.__setattr__(inst, self.attribute, {})
+            return object.__getattribute__(inst, self.attribute)
         return val
 
     def __delete__(self, inst):
-        setattr(inst, self.attribute, {})
+        object.__setattr__(inst, self.attribute, {})
 
 
 @implementer(IBaseObject)
@@ -42,12 +42,22 @@ class BaseObject(object):
 
     def __new__(cls, *args, **kw):
         inst = super(BaseObject, cls).__new__(cls)
-        inst._BaseObject__annotations = {}
+        object.__setattr__(inst, '_BaseObject__annotations', {})
+        object.__setattr__(inst, '_BaseObject__jar', None)
+        object.__setattr__(inst, '_BaseObject__oid', None)
+        object.__setattr__(inst, '_BaseObject__serial', None)
+        object.__setattr__(inst, '_BaseObject__new_marker', False)
+        object.__setattr__(inst, '_BaseObject__parent', None)
+        object.__setattr__(inst, '_BaseObject__of', None)
+        object.__setattr__(inst, '_BaseObject__name', None)
+        object.__setattr__(inst, '_BaseObject__immutable_cache', False)
         return inst
 
     def __repr__(self):
         return "<%s %d>" % (self.__class__.__name__, id(self))
 
+    __slots__ = ('__parent', '__of', '__name', '__annotations', '__immutable_cache',
+                 '__new_marker', '__jar', '__oid', '__serial')
     __parent__ = ObjectProperty('_BaseObject__parent', None)
     __of__ = ObjectProperty('_BaseObject__of', None)
     __name__ = ObjectProperty('_BaseObject__name', None)
@@ -72,20 +82,4 @@ class BaseObject(object):
 
     @profilable
     def __getstate__(self):
-        idict = getattr(self, '__dict__', None)
-        if idict is not None:
-            d = dict([x for x in idict.items()
-                      if not x[0].startswith('_BaseObject__')])
-        else:
-            d = None
-        return d
-
-    @profilable
-    def __setstate__(self, state):
-        if isinstance(state, tuple):
-            inst_dict = state[0]
-        else:
-            inst_dict = state
-        idict = getattr(self, '__dict__', None)
-        if inst_dict is not None:
-            idict.update(inst_dict)
+        return self.__dict__
