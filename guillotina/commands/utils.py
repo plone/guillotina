02ff52214f0fc8e69297aade15b78caf0e652907
vangheem <vangheem@gmail.com>
2018-04-04 20:28:48 -0400

    Back to development: 3.0.7

--- guillotina/commands/utils.py
+++ guillotina/commands/utils.py
@@ -7,4 +7,4 @@ def change_transaction_strategy(strategy='none'):
     root = get_utility(IApplication, name='root')
     for _id, db in root:
         if IDatabase.providedBy(db):
-            db._db._storage._transaction_strategy = strategy
+            db._storage._transaction_strategy = strategy
