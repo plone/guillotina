b47856df4e1d0cebee1cbd775fa881ecdc4a283c
vangheem <vangheem@gmail.com>
2018-05-10 19:42:28 +0200

    Be able to modify file attributes filename, content_type, md5, extension

--- guillotina/tests/test_attachment.py
+++ guillotina/tests/test_attachment.py
@@ -15,7 +15,7 @@ async def test_create_content_with_behavior(container_requester):
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -46,7 +46,7 @@ async def test_large_upload_chunks(container_requester):
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -86,7 +86,7 @@ async def test_tus(container_requester):
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -150,7 +150,7 @@ async def test_tus_unknown_size(container_requester):
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -222,12 +222,12 @@ async def test_tus_unknown_size(container_requester):
 
 async def test_copy_file_ob(container_requester):
     async with container_requester as requester:
-        response, status = await requester(
+        _, status = await requester(
             'POST',
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -262,12 +262,12 @@ async def test_copy_file_ob(container_requester):
 
 async def test_tus_unfinished_error(container_requester):
     async with container_requester as requester:
-        response, status = await requester(
+        _, status = await requester(
             'POST',
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -319,7 +319,7 @@ async def test_tus_with_empty_file(container_requester):
             '/db/guillotina/',
             data=json.dumps({
                 '@type': 'Item',
-                '@behaviors': ['guillotina.behaviors.attachment.IAttachment'],
+                '@behaviors': [IAttachment.__identifier__],
                 'id': 'foobar'
             })
         )
@@ -372,3 +372,49 @@ async def test_tus_with_empty_file(container_requester):
             behavior = IAttachment(obj)
             await behavior.load()
             assert behavior.file._blob.chunks == 0
+
+
+async def test_update_filename_on_files(container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            'POST',
+            '/db/guillotina/',
+            data=json.dumps({
+                '@type': 'Item',
+                '@behaviors': [IAttachment.__identifier__],
+                'id': 'foobar'
+            })
+        )
+        assert status == 201
+
+        response, status = await requester(
+            'PATCH',
+            '/db/guillotina/foobar/@upload/file',
+            data=b'X' * 1024 * 1024,
+            headers={
+                'x-upload-size': str(1024 * 1024)
+            }
+        )
+        assert status == 200
+
+        response, status = await requester(
+            'PATCH',
+            '/db/guillotina/foobar',
+            data=json.dumps({
+                IAttachment.__identifier__: {
+                    "file": {
+                        "filename": "foobar.jpg",
+                        "content_type": "image/jpeg",
+                        "md5": 'foobar',
+                        'extension': 'jpg'
+                    }
+                }
+            })
+        )
+
+        response, status = await requester('GET', '/db/guillotina/foobar')
+        data = response[IAttachment.__identifier__]['file']
+        assert data['filename'] == 'foobar.jpg'
+        assert data['content_type'] == 'image/jpeg'
+        assert data['md5'] == 'foobar'
+        assert data['extension'] == 'jpg'
