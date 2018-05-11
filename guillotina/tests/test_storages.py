af8f71ecf54212b13ad1f1a36bf36cecc985a667
vangheem <vangheem@gmail.com>
2018-05-11 15:30:27 +0200

    Fix dynamic storage urls for cockroach

--- guillotina/tests/test_storages.py
+++ guillotina/tests/test_storages.py
@@ -1,3 +1,8 @@
+from guillotina._settings import app_settings
+from guillotina.component import get_adapter
+from guillotina.db.factory import CockroachDatabaseManager
+from guillotina.db.interfaces import IDatabaseManager
+
 import json
 
 
@@ -47,3 +52,24 @@ async def test_delete_database(container_requester):
         assert status == 200
         response, status = await requester('GET', '/@storages/db')
         assert 'foobar' not in response['databases']
+
+
+async def test_storage_impl(db, guillotina_main):
+    storages = app_settings['storages']
+    storage_config = storages['db']
+    factory = get_adapter(guillotina_main.root, IDatabaseManager,
+                          name=storage_config['storage'],
+                          args=[storage_config])
+    assert len(await factory.get_names()) == 0
+    await factory.create('foobar')
+    assert len(await factory.get_names()) == 1
+    await factory.delete('foobar')
+    assert len(await factory.get_names()) == 0
+
+
+async def test_get_dsn_from_url():
+    factory = CockroachDatabaseManager(None, {
+        'dsn': 'postgresql://root@127.0.0.1:26257?sslmode=disable'
+    })
+    assert (factory.get_dsn('foobar') ==
+        'postgresql://root@127.0.0.1:26257/foobar?sslmode=disable')
