aa870402a850867529cf3e64714d6e1f353e9aaf
vangheem <vangheem@gmail.com>
2018-05-11 15:49:23 +0200

    fix!

--- guillotina/tests/test_storages.py
+++ guillotina/tests/test_storages.py
@@ -4,6 +4,11 @@ from guillotina.db.factory import CockroachDatabaseManager
 from guillotina.db.interfaces import IDatabaseManager
 
 import json
+import os
+import pytest
+
+
+DATABASE = os.environ.get('DATABASE', 'DUMMY')
 
 
 async def test_get_storages(container_requester):
@@ -54,6 +59,7 @@ async def test_delete_database(container_requester):
         assert 'foobar' not in response['databases']
 
 
+@pytest.mark.skipif(DATABASE == 'DUMMY', reason='Not for dummy db')
 async def test_storage_impl(db, guillotina_main):
     storages = app_settings['storages']
     storage_config = storages['db']
