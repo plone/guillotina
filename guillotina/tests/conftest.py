3a2e0202311b1a435cf4e801ee031a308d18bb25
vangheem <vangheem@gmail.com>
2018-05-20 07:29:50 -0400

    need to test with newer cockroachdb

--- guillotina/tests/conftest.py
+++ guillotina/tests/conftest.py
@@ -1,3 +1,11 @@
+from pytest_docker_fixtures import images
+
+
+images.configure(
+    'cockroach',
+    'cockroachdb/cockroach', 'v2.0.1')
+
+
 pytest_plugins = [
     'guillotina.tests.fixtures'
 ]
