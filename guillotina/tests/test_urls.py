51f63066af74e0e0710d18c25b4b6477121e947e
vangheem <vangheem@gmail.com>
2019-01-11 23:22:56 -0500

    pay attention to forwarded headers in urls

---
+++ guillotina/tests/test_urls.py
@@ -0,0 +1,55 @@
+from guillotina.tests.utils import make_mocked_request
+from guillotina.utils import get_url
+import json
+
+
+def test_vhm_url(dummy_guillotina):
+    request = make_mocked_request('GET', '/', {
+        'X-VirtualHost-Monster': 'https://foobar.com/foo/bar'
+    })
+
+    assert get_url(
+        request, '/c/d') == 'https://foobar.com/foo/bar/c/d'
+
+
+def test_forwarded_proto_url(dummy_guillotina):
+    request = make_mocked_request('GET', '/', {
+        'X-Forwarded-Proto': 'https'
+    })
+    url = get_url(request, '/c/d')
+    assert url.startswith('https://')
+    assert url.endswith('/c/d')
+
+
+async def test_url_of_object_with_vhm(container_requester):
+    async with container_requester as requester:
+        resp, _ = await requester(
+            'POST',
+            '/db/guillotina/',
+            data=json.dumps({
+                '@type': 'Item'
+            }),
+            headers={
+                'X-VirtualHost-Monster': 'https://foobar.com/foo/bar'
+            }
+        )
+        assert resp['@id'].startswith(
+            'https://foobar.com/foo/bar/db/guillotina')
+
+
+async def test_url_of_object_with_scheme(container_requester):
+    async with container_requester as requester:
+        resp, _ = await requester(
+            'POST',
+            '/db/guillotina/',
+            data=json.dumps({
+                '@type': 'Item',
+                'id': 'foobar'
+            }),
+            headers={
+                'X-VirtualHost-Monster': 'https://foobar.com/foo/bar'
+            }
+        )
+        url = resp['@id']
+        assert url.startswith('https://')
+        assert url.endswith('/db/guillotina/foobar')
--- guillotina/utils/misc.py
+++ guillotina/utils/misc.py
@@ -262,3 +262,25 @@ def safe_unidecode(val: bytes) -> str:
         except UnicodeDecodeError:
             pass
     return val.decode('utf-8', errors='replace')
+
+
+def get_url(req, path):
+    '''
+    Return calculated url from a request object taking
+    into account X-VirtualHost-Monster header
+    '''
+    if 'X-VirtualHost-Monster' in req.headers:
+        virtualhost = req.headers['X-VirtualHost-Monster']
+    else:
+        virtualhost = None
+
+    if virtualhost:
+        return '{}/{}'.format(virtualhost.rstrip('/'), path.strip('/'))
+    else:
+        url = req.url.with_path(path)
+        for hdr in ('X-Forwarded-Proto', 'X-Forwarded-Scheme',):
+            forwarded_proto = req.headers.get(hdr, None)
+            if forwarded_proto:
+                url = url.with_scheme(forwarded_proto)
+                break
+        return str(url)
