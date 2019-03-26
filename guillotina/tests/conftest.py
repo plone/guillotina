3732f36ee0baf8233fb230315a813d94070e0401
vangheem <vangheem@gmail.com>
2019-03-26 15:04:25 -0400

    bump cr in tests
    Co-authored-by: Wildcard Corp. <corporate@wildcardcorp.com>

--- guillotina/tests/conftest.py
+++ guillotina/tests/conftest.py
@@ -3,7 +3,7 @@ from pytest_docker_fixtures import images
 
 images.configure(
     'cockroach',
-    'cockroachdb/cockroach', 'v2.0.1')
+    'cockroachdb/cockroach', 'v2.1.6')
 
 
 pytest_plugins = [
