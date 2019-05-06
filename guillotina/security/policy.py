17b0cffa05a9e8b72010c1680c6ace68a9c234ed
vangheem <vangheem@gmail.com>
2019-05-06 17:16:17 -0400

    fix

--- guillotina/security/policy.py
+++ guillotina/security/policy.py
@@ -156,7 +156,7 @@ class Interaction(object):
 
     def cache(self, parent, level=''):
         serial = getattr(parent, '_p_serial', '')
-        cache_key = f'{id(parent)}-{serial}'
+        cache_key = f'{id(parent)}-{serial}-{level}'
         cache = self._cache.get(cache_key)
         if cache is None:
             cache = CacheEntry()
