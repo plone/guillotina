b683d6f935e34b8646a377063ddbe84196fd6879
Ferran Llamas <llamas.arroniz@gmail.com>
2019-06-16 12:14:39 +0200

    wip: purge

--- guillotina/api/httpcache.py
+++ guillotina/api/httpcache.py
@@ -1,10 +1,17 @@
 from zope.interface import Interface
+from guillotina.utils import execute
+import aiohttp
+import asyncio
 
 
 class IHttpCachePolicyUtility(Interface):
     def __call__(context, request):
         """
         Returns a dictionary with the headers to be added on the response
+        """
+
+    async def purge(context):
+        """Purges previous responses from all configured proxy servers
         """
 
 
@@ -16,11 +23,23 @@ class NoHttpCachePolicyUtility:
         # No headers in this case
         return None
 
+    async def pruge(self, context):
+        # Nothing to purge
+        return
+
 
 class SimpleHttpCachePolicyUtility:
     def __init__(self, settings, loop=None):
+        self.loop = loop or asyncio.get_event_loop()
+        self.proxy_servers = settings.get('proxy_servers', [])
         self.max_age = settings.get('max_age')
         self.public = settings.get('public', False)
+
+    def get_etag(self, context):
+        # TODO: get tid from context vars
+        tid = 'foo'
+        uuid = getattr(context, "uuid", '0')  # 0 for app root object
+        return f'{uuid}/{tid}'
 
     def __call__(self, context, request):
         cache_control = 'no-cache'
@@ -28,9 +47,34 @@ class SimpleHttpCachePolicyUtility:
             cache_control = f'max-age={self.max_age}'
         publicstr = 'public' if self.public else 'private'
         cache_control += f', {publicstr}'
-        # Etag 0 for application root
-        uuid = getattr(context, "uuid", '0')
         return {
             'Cache-Control': cache_control,
-            "ETag": uuid
+            "ETag": self.get_etag(context)
         }
+
+    async def purge(self, context):
+        execute.in_pool(
+            self._real_purge,
+            context).after_request()
+
+    async def _real_purge(self, context):
+        async with aiohttp.ClientSession() as session:
+            tasks = []
+            for pserver in self.proxy_servers:
+                task = self.purge_from_proxy(pserver, context, session)
+                tasks.append(task)
+            # Wait for tasks to finish
+            await asyncio.gather(*tasks)
+
+    def purge_from_proxy(self, proxy, context, session):
+        task = asyncio.ensure_future(
+            self._purge_from_proxy(proxy, context, session),
+            loop=self.loop)
+        return task
+
+    async def _purge_from_proxy(self, proxy, context, session):
+        # TODO: get absolute url from context
+        url = f'http://{proxy}/{context.url}'
+        async with session.purge(url) as resp:
+            if resp.status != 200:
+                raise Exception('Unable to purge')
