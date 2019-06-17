6fe410660693ae9707aa9d1a1ab6b6f2ec323fbd
vangheem <vangheem@gmail.com>
2019-06-17 08:20:57 +0200

    input could be int for example

--- guillotina/contrib/catalog/pg.py
+++ guillotina/contrib/catalog/pg.py
@@ -80,6 +80,8 @@ def sqlq(v):
     We use sql arguments where we don't control the information but let's
     be extra careful anyways...
     """
+    if not isinstance(v, (bytes, str)):
+        return v
     for value, replacement in _sql_replacements:
         v = v.replace(value, replacement)
     return v
