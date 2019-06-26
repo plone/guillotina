40a4f62577fc07fd8b2735280a91767fefcc617c
vangheem <vangheem@gmail.com>
2019-06-26 15:55:53 -0400

    fix

--- guillotina/tests/test_catalog.py
+++ guillotina/tests/test_catalog.py
@@ -115,9 +115,9 @@ async def test_get_data_uses_indexes_param(dummy_guillotina):
     container.__name__ = 'guillotina'
     ob = await create_content('Item', id='foobar')
     data = await util.get_data(ob, indexes=['title'])
-    assert len(data) == 4  # @uid, type_name, etc always returned
+    assert len(data) == 7  # @uid, type_name, etc always returned
     data = await util.get_data(ob, indexes=['title', 'id'])
-    assert len(data) == 5
+    assert len(data) == 8
 
     data = await util.get_data(ob)
     assert len(data) > 9
@@ -138,12 +138,12 @@ async def test_modified_event_gathers_all_index_data(dummy_guillotina):
     }))
     fut = index.get_indexer()
 
-    assert len(fut.update['foobar']) == 5
+    assert len(fut.update['foobar']) == 8
 
     await notify(ObjectModifiedEvent(ob, payload={
         'creation_date': ''
     }))
-    assert len(fut.update['foobar']) == 6
+    assert len(fut.update['foobar']) == 9
 
 
 @pytest.mark.app_settings(PG_CATALOG_SETTINGS)
