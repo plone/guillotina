87a0d329b290622f499e10718aadb3f541697e99
Jordi Masip <jordi@masip.cat>
2019-10-10 12:23:18 +0200

    asdf

--- guillotina/db/storages/pg.py
+++ guillotina/db/storages/pg.py
@@ -23,7 +23,9 @@ import time
 import ujson
 
 
-log = logging.getLogger("guillotina.storage")
+from guillotina import glogging
+
+log = glogging.getLogger("guillotina.storage")
 
 
 # we can not use FOR UPDATE or FOR SHARE unfortunately because
@@ -783,22 +785,22 @@ WHERE tablename = '{}' AND indexname = '{}_parent_id_id_key';
     @restart_conn_on_exception
     async def open(self):
         qsize = self.pool._queue.qsize()
-        log.debug(f"MRK BeforeOpen: {qsize}")
+        log.info(f"MRK BeforeOpen: {qsize}")
         conn = await self.pool.acquire(timeout=self._conn_acquire_timeout)
-        log.debug(f"MRK Open {conn}")
+        log.info(f"MRK Open {conn}")
         return conn
 
     async def close(self, con):
-        log.debug(f"MRK Close {con}", exc_info=True)
+        log.info(f"MRK Close {con}", exc_info=True)
         try:
-            await shield(asyncio.wait_for(self.pool.release(con, timeout=1), 1))
-        except (RuntimeError, asyncio.TimeoutError):
-            log.debug(f"MRK Close Exc {con}", exc_info=True)
+            await shield(self.pool.release(con, timeout=1))
+        except (RuntimeError, asyncio.TimeoutError,):
+            log.info(f"MRK Close Exc {con}", exc_info=True)
         except (asyncio.CancelledError, asyncpg.exceptions.ConnectionDoesNotExistError):
-            pass
+            log.info(f"MRK Close Cancelled {con}", exc_info=True)
 
     async def terminate(self, conn):
-        log.debug(f"MRK Terminate {conn}", exc_info=True)
+        log.info(f"MRK Terminate {conn}", exc_info=True)
         conn.terminate()
 
     async def load(self, txn, oid):
