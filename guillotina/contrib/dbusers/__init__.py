47e0bc1aeaec55b3d3e3d7caf7f00e3a47cf8ba6
Ferran Llamas <llamas.arroniz@gmail.com>
2019-10-22 12:47:58 +0200

    cleanup unused permissions and roles

--- guillotina/contrib/dbusers/__init__.py
+++ guillotina/contrib/dbusers/__init__.py
@@ -7,16 +7,6 @@ _ = MessageFactory("guillotina.contrib.dbusers")
 
 app_settings = {"auth_user_identifiers": ["guillotina.contrib.dbusers.users.DBUserIdentifier"]}
 
-configure.permission("guillotina.NotAuthenticated", "")
-configure.permission("guillotina.Authenticated", "")
-configure.permission("guillotina.AddUser", title="Add plone user")
-configure.permission("guillotina.AddGroup", title="Add plone group")
-configure.grant(permission="guillotina.AccessContent", role="guillotina.Anonymous")
-configure.grant(permission="guillotina.NotAuthenticated", role="guillotina.Anonymous")
-configure.grant(permission="guillotina.Authenticated", role="guillotina.Authenticated")
-configure.grant(permission="guillotina.AddUser", role="guillotina.ContainerAdmin")
-configure.grant(permission="guillotina.AddGroup", role="guillotina.ContainerAdmin")
-
 
 def includeme(root, settings):
     configure.scan("guillotina.contrib.dbusers.content.users")
