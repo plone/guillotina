8c1a83a2b3c450239d013030d40e32c6db0ae3d1
vangheem <vangheem@gmail.com>
2019-10-30 07:54:22 -0400

    Handle empty `G_` environment variable values

--- guillotina/commands/__init__.py
+++ guillotina/commands/__init__.py
@@ -93,7 +93,7 @@ def get_settings(configuration, overrides=None):
             continue
         name = env_name[2:]
         value = os.environ[orig_env_name]
-        if value[0] in ("{", "["):
+        if len(value) > 0 and value[0] in ("{", "["):
             value = json.loads(value)
         context = settings
         parts = name.split("__")
