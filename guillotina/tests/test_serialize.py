b5ac0fdbddd7d76b07509611050c398846d8c1ee
vangheem <vangheem@gmail.com>
2019-11-01 09:43:52 -0400

    mypy

--- guillotina/tests/test_serialize.py
+++ guillotina/tests/test_serialize.py
@@ -16,10 +16,10 @@ from guillotina.tests.utils import create_content
 from guillotina.tests.utils import login
 from zope.interface import Interface
 from zope.interface import Invalid
-from zope.interface import invariant
 
 import pytest
 import uuid
+import zope.interface
 
 
 async def test_serialize_resource(dummy_request, mock_txn):
@@ -815,7 +815,7 @@ class ITestValidation(Interface):
 
     validated_text = schema.Text(required=False)
 
-    @invariant
+    @zope.interface.invariant
     def text_should_not_be_foobar(ob):
         if getattr(ob, "foo", None) == "foo" and getattr(ob, "bar", None) == "bar":
             raise Invalid(ob)
