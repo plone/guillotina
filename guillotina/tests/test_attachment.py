130968eae5135801e39f4f44195ca57747976565
Nathan Van Gheem <vangheem@gmail.com>
2020-01-02 10:13:04 -0500

    [5.x] Add `Range` header support (#803)

--- guillotina/tests/test_attachment.py
+++ guillotina/tests/test_attachment.py
@@ -6,6 +6,9 @@ from guillotina.interfaces import IFileManager
 from guillotina.tests import utils
 from guillotina.transactions import transaction
 
+import asyncio
+import base64
+import hashlib
 import json
 import pytest
 import random
@@ -546,6 +549,7 @@ async def test_file_head(manager_type, redis_container, container_requester):
         assert status == 200
         assert headers["Content-Length"] == str(1024 * 1024 * 4)
         assert headers["Content-Type"] == "application/octet-stream"
+        assert headers["Accept-Ranges"] == "bytes"
 
 
 @pytest.mark.parametrize("manager_type", _pytest_params)
@@ -562,3 +566,302 @@ async def test_file_head_not_found(manager_type, redis_container, container_requ
             "HEAD", "/db/guillotina/foobar/@download/file", accept="application/json"
         )
         assert status == 404
+
+
+@pytest.mark.parametrize("manager_type", _pytest_params)
+async def test_range_request_download(manager_type, redis_container, container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/",
+            data=json.dumps({"@type": "Item", "@behaviors": [IAttachment.__identifier__], "id": "foobar"}),
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={"UPLOAD-LENGTH": str(1024 * 1024 * 10), "TUS-RESUMABLE": "1.0.0"},
+        )
+        assert status == 201
+
+        for idx in range(10):
+            # 10, 1mb chunks
+            response, status = await requester(
+                "PATCH",
+                "/db/guillotina/foobar/@tusupload/file",
+                headers={
+                    "CONTENT-LENGTH": str(1024 * 1024 * 1),
+                    "TUS-RESUMABLE": "1.0.0",
+                    "upload-offset": str(1024 * 1024 * idx),
+                },
+                data=str(idx).encode("ascii") * 1024 * 1024 * 1,
+            )
+            assert status == 200
+
+        response, status, headers = await requester.make_request(
+            "GET",
+            "/db/guillotina/foobar/@download/file",
+            accept="application/json",
+            headers={"Range": f"bytes={1024 * 1024}-{(2 * 1024 * 1024) - 1}"},
+        )
+
+        assert headers["Content-Range"] == f"bytes {1024 * 1024}-{(2 * 1024 * 1024) - 1}/{10 * 1024 * 1024}"
+        assert len(response) == 1024 * 1024
+        assert response == b"1" * 1024 * 1024
+
+        response, status, headers = await requester.make_request(
+            "GET",
+            "/db/guillotina/foobar/@download/file",
+            headers={"Range": f"bytes={(1024 * 1024 * 3) + 512}-{((5 * 1024 * 1024) + 512) - 1}"},
+        )
+
+        assert len(response) == 2 * 1024 * 1024
+        assert response == (b"3" * ((1024 * 1024) - 512)) + (b"4" * 1024 * 1024) + (b"5" * 512)
+
+        # test whole range
+        response, status, headers = await requester.make_request(
+            "GET",
+            "/db/guillotina/foobar/@download/file",
+            accept="application/json",
+            headers={"Range": f"bytes=0-"},
+        )
+
+        assert headers["Content-Range"] == f"bytes 0-{(10 * 1024 * 1024) - 1}/{10 * 1024 * 1024}"
+        assert len(response) == 10 * 1024 * 1024
+
+
+@pytest.mark.parametrize("manager_type", _pytest_params)
+async def test_invalid_range_request_download(manager_type, redis_container, container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/",
+            data=json.dumps({"@type": "Item", "@behaviors": [IAttachment.__identifier__], "id": "foobar"}),
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={"UPLOAD-LENGTH": str(1024 * 1024 * 2), "TUS-RESUMABLE": "1.0.0"},
+        )
+        assert status == 201
+
+        for idx in range(2):
+            # 2, 1mb chunks
+            response, status = await requester(
+                "PATCH",
+                "/db/guillotina/foobar/@tusupload/file",
+                headers={
+                    "CONTENT-LENGTH": str(1024 * 1024 * 1),
+                    "TUS-RESUMABLE": "1.0.0",
+                    "upload-offset": str(1024 * 1024 * idx),
+                },
+                data=str(idx).encode("ascii") * 1024 * 1024 * 1,
+            )
+            assert status == 200
+
+        _, status, _ = await requester.make_request(
+            "GET", "/db/guillotina/foobar/@download/file", headers={"Range": f"foobar=0-2"}
+        )
+        assert status == 416
+
+        _, status, _ = await requester.make_request(
+            "GET", "/db/guillotina/foobar/@download/file", headers={"Range": f"bytes=zero-2"}
+        )
+        assert status == 416
+
+        _, status, _ = await requester.make_request(
+            "GET", "/db/guillotina/foobar/@download/file", headers={"Range": f"bytes=5-2"}
+        )
+        assert status == 416
+
+        _, status, _ = await requester.make_request(
+            "GET",
+            "/db/guillotina/foobar/@download/file",
+            headers={"Range": f"bytes={(1024 * 1024 * 5)}-{(1024 * 1024 * 7)}"},
+        )
+        assert status == 416
+
+
+@pytest.mark.parametrize("manager_type", _pytest_params)
+async def _test_handle_cancel_error(manager_type, redis_container, container_requester):  # pragma: no cover
+    # it seems we can't test cancelled errors with pytest
+    async with container_requester as requester:
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/",
+            data=json.dumps({"@type": "Item", "@behaviors": [IAttachment.__identifier__], "id": "foobar"}),
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={"UPLOAD-LENGTH": str(1024 * 1024 * 3), "TUS-RESUMABLE": "1.0.0"},
+        )
+        assert status == 201
+
+        for idx in range(3):
+            # 10, 1mb chunks
+            response, status = await requester(
+                "PATCH",
+                "/db/guillotina/foobar/@tusupload/file",
+                headers={
+                    "CONTENT-LENGTH": str(1024 * 1024 * 1),
+                    "TUS-RESUMABLE": "1.0.0",
+                    "upload-offset": str(1024 * 1024 * idx),
+                },
+                data=str(idx).encode("ascii") * 1024 * 1024 * 1,
+            )
+            assert status == 200
+
+        task = asyncio.create_task(
+            requester.make_request(
+                "GET",
+                "/db/guillotina/foobar/@download/file",
+                accept="application/json",
+                headers={"Range": f"bytes=0-"},
+            )
+        )
+        await asyncio.sleep(0.005)
+        task.cancel()
+        _, status, _ = await task
+
+
+@pytest.mark.parametrize("manager_type", _pytest_params)
+async def test_tus_errors(manager_type, redis_container, container_requester):
+    # it seems we can't test cancelled errors with pytest
+    async with container_requester as requester:
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/",
+            data=json.dumps({"@type": "Item", "@behaviors": [IAttachment.__identifier__], "id": "foobar"}),
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST", "/db/guillotina/foobar/@tusupload/file", headers={"UPLOAD-LENGTH": str(1024 * 1024 * 3)}
+        )
+        assert status == 412
+
+        response, status = await requester(
+            "POST", "/db/guillotina/foobar/@tusupload/file", headers={"TUS-RESUMABLE": "1.0.0"}
+        )
+        assert status == 412
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={
+                "UPLOAD-LENGTH": str(1024 * 1024 * 3),
+                "TUS-RESUMABLE": "1.0.0",
+                "UPLOAD-MD5": hashlib.md5(b"0" * 1024 * 1024 * 3).hexdigest(),
+                "UPLOAD-EXTENSION": "jpg",
+                "X-UPLOAD-FILENAME": "foobar.jpg",
+            },
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={
+                "UPLOAD-LENGTH": str(1024 * 1024 * 3),
+                "TUS-RESUMABLE": "1.0.0",
+                "UPLOAD-FILENAME": "foobar.jpg",
+                "TUS-OVERRIDE-UPLOAD": "1",
+            },
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={
+                "UPLOAD-LENGTH": str(1024 * 1024 * 3),
+                "TUS-RESUMABLE": "1.0.0",
+                "UPLOAD-METADATA": "foo " + base64.b64encode(b"foobar.jpg").decode("utf-8"),
+                "TUS-OVERRIDE-UPLOAD": "1",
+            },
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={
+                "CONTENT-LENGTH": str(1024 * 1024 * 1),
+                "TUS-RESUMABLE": "1.0.0",
+                "X-HTTP-Method-Override": "PATCH",
+            },
+            data=b"0" * 1024 * 1024 * 1,
+        )
+        assert status == 412
+
+        response, status = await requester(
+            "PATCH",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={"CONTENT-LENGTH": str(1024 * 1024 * 1), "TUS-RESUMABLE": "1.0.0", "upload-offset": "0"},
+            data=b"0" * 1024 * 1024 * 1,
+        )
+        assert status == 200
+
+        response, status = await requester(
+            "PATCH",
+            "/db/guillotina/foobar/@tusupload/file",
+            headers={"CONTENT-LENGTH": str(1024 * 1024 * 1), "TUS-RESUMABLE": "1.0.0", "upload-offset": "0"},
+            data=b"1" * 1024 * 1024 * 1,
+        )
+        assert status == 409
+
+
+@pytest.mark.parametrize("manager_type", _pytest_params)
+async def test_upload_errors(manager_type, redis_container, container_requester):
+    async with container_requester as requester:
+        response, status = await requester(
+            "POST",
+            "/db/guillotina/",
+            data=json.dumps({"@type": "Item", "@behaviors": [IAttachment.__identifier__], "id": "foobar"}),
+        )
+        assert status == 201
+
+        response, status = await requester(
+            "PATCH",
+            "/db/guillotina/foobar/@upload/file",
+            data=b"X" * 1024 * 1024 * 4,
+            headers={
+                "content-length": str(1024 * 1024 * 4),
+                "X-UPLOAD-MD5HASH": hashlib.md5(b"X" * 1024 * 1024 * 4).hexdigest(),
+                "X-UPLOAD-EXTENSION": "bin",
+                "X-UPLOAD-FILENAME": "foo.bin",
+            },
+        )
+        assert status == 200
+
+        response, status = await requester(
+            "PATCH",
+            "/db/guillotina/foobar/@upload/file",
+            data=b"X" * 1024 * 1024 * 4,
+            headers={
+                "content-length": str(1024 * 1024 * 4),
+                "UPLOAD-MD5": hashlib.md5(b"X" * 1024 * 1024 * 4).hexdigest(),
+                "X-UPLOAD-EXTENSION": "bin",
+                "X-UPLOAD-FILENAME-B64": base64.b64encode(b"foobar.bin").decode("utf-8"),
+            },
+        )
+        assert status == 200
+
+        response, status = await requester(
+            "PATCH",
+            "/db/guillotina/foobar/@upload/file",
+            data=b"X" * 1024 * 1024 * 4,
+            headers={
+                "x-upload-size": str(1024 * 1024 * 1),
+                "UPLOAD-MD5": hashlib.md5(b"X" * 1024 * 1024 * 4).hexdigest(),
+                "X-UPLOAD-EXTENSION": "bin",
+                "X-UPLOAD-FILENAME-B64": base64.b64encode(b"foobar.bin").decode("utf-8"),
+            },
+        )
+        assert status == 412
