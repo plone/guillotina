b01f6bdd54b50a94a366ebc585b495fdbbaaf607
Jordi Masip <masipcat@users.noreply.github.com>
2020-01-18 13:24:27 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
             "pytest-rerunfailures<=7.0",
             "async-asgi-testclient~=1.2.0",
             "openapi-spec-validator",
+            "aiohttp>=3.0.0,<4.0.0",
         ],
         "docs": [
             "aiohttp>=3.0.0,<4.0.0",
