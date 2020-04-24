cc59b09965437da455a4eb9d9677fdf9f150a3a9
vangheem <vangheem@gmail.com>
2020-04-24 09:12:07 -0400

    fix

--- guillotina/db/interfaces.py
+++ guillotina/db/interfaces.py
@@ -328,7 +328,6 @@ class IStorage(Interface):
 
 class IPostgresStorage(IStorage):
     objects_table_name = Attribute("")
-    blobs_table_name = Attribute("")
     sql = Attribute("")
     pool = Attribute("")
 
