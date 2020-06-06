de968f78e35d8e9d733c09537461f2586829bfdb
vangheem <vangheem@gmail.com>
2020-06-06 05:53:37 -0400

    fix mypy

--- stubs/jsonschema/validators/__init__.py
+++ stubs/jsonschema/validators/__init__.py
@@ -2,11 +2,20 @@ from typing import Any
 from typing import Dict
 
 
+class RefResolver:
+    @classmethod
+    def from_schema(cls, schema) -> "RefResolver":
+        ...
+
+
 class Validator:
+    def __init__(self):
+        ...
+
     def check_schema(self, schema: Dict[str, Any]):
         ...
 
-    def __call__(self, schema: Dict[str, Any]) -> "Validator":
+    def __call__(self, schema: Dict[str, Any], resolver: RefResolver) -> "Validator":
         ...
 
 
