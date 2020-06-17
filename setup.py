79d396ed24ee6bd0fbe041489a40a526a10b1b07
Pau Freixes <pfreixes@gmail.com>
2020-06-17 14:16:37 +0200

    Try to pin the pytest-cov package

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
             "backoff",
             "psycopg2-binary",
             "pytest-asyncio==0.10.0",
-            "pytest-cov",
+            "pytest-cov>=2.0.0<=2.9.0",
             "coverage>=4.0.3",
             "pytest-docker-fixtures",
             "pytest-rerunfailures<=7.0",
