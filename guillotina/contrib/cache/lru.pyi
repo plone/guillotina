8229442080d29a3a30fbbe65a262970ee251ec25
Nathan Van Gheem <vangheem@gmail.com>
2020-09-30 13:32:08 -0400

    Add metrics for in-memory cache (#1014)

--- guillotina/contrib/cache/lru.pyi
+++ guillotina/contrib/cache/lru.pyi
@@ -1,8 +1,11 @@
-from typing import Any
+from typing import Any, Callable
 from typing import Dict
 from typing import Optional
 
 
 class LRU(Dict[str, Any]):
-    def __init__(self, size: int): ...
-    def set(self, key: str, value: Any, size: Optional[int] = None) -> None: ...
+    def __init__(self, size: int, callback: Callable[[str, Any], Any]):
+        ...
+
+    def set(self, key: str, value: Any, size: Optional[int] = None) -> None:
+        ...
--- guillotina/contrib/cache/memcache.py
+++ guillotina/contrib/cache/memcache.py
@@ -3,12 +3,35 @@ from guillotina.contrib.cache.lru import LRU
 from typing import Optional
 
 
+try:
+    import prometheus_client
+
+    MEMORY_OPS = prometheus_client.Counter(
+        "guillotina_cache_memory_ops_total",
+        "Total count of ops by type of operation and the error if there was.",
+        labelnames=["type", "result"],
+    )
+
+    def record_memory_op(type: str, result: str) -> None:
+        MEMORY_OPS.labels(type=type, result=result).inc()
+
+
+except ImportError:
+
+    def record_memory_op(type: str, result: str) -> None:
+        ...
+
+
 _lru: Optional[LRU] = None
+
+
+def _evicted(key, value):
+    record_memory_op("evicted", "none")
 
 
 def get_memory_cache() -> LRU:
     global _lru
     if _lru is None:
         settings = app_settings.get("cache", {"memory_cache_size": 209715200})
-        _lru = LRU(settings["memory_cache_size"])
+        _lru = LRU(settings["memory_cache_size"], callback=_evicted)
     return _lru
