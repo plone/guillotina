8f59038d8ed17403e24496af0a45bcdb8a0a059e
Ramon Navarro Bosch <ramon.nb@gmail.com>
2020-11-15 22:55:34 +0100

    Fix Sort

--- guillotina/contrib/workflows/subscriber.py
+++ guillotina/contrib/workflows/subscriber.py
@@ -1,18 +1,16 @@
-import datetime
-
 from guillotina import configure
 from guillotina.component import query_adapter
+from guillotina.contrib.workflows.interfaces import IWorkflow
+from guillotina.contrib.workflows.interfaces import IWorkflowBehavior
 from guillotina.interfaces import IObjectAddedEvent
 from guillotina.interfaces import IResource
 from guillotina.security.utils import apply_sharing
 from guillotina.utils import get_authenticated_user_id
-from guillotina.contrib.workflows.interfaces import IWorkflowBehavior
-from guillotina.contrib.workflows.interfaces import IWorkflow
+
+import datetime
 
 
-@configure.subscriber(
-    for_=(IResource, IObjectAddedEvent),
-    priority=1001)  # after indexing
+@configure.subscriber(for_=(IResource, IObjectAddedEvent), priority=1001)  # after indexing
 async def workflow_object_added(obj, event):
     workflow = query_adapter(obj, IWorkflowBehavior)
     if workflow is not None:
@@ -22,21 +20,18 @@ async def workflow_object_added(obj, event):
         await workflow.load(create=True)
         state = workflow.review_state
 
-        if 'set_permission' in wkf.states[state]:
-            await apply_sharing(obj, wkf.states[state]['set_permission'])
+        if "set_permission" in wkf.states[state]:
+            await apply_sharing(obj, wkf.states[state]["set_permission"])
 
-        setattr(workflow, 'history', [])
+        setattr(workflow, "history", [])
         workflow.history.append(
             {
-                'actor': user_id,
-                'comments': '',
-                'time': datetime.datetime.now(),
-                'title': 'Created',
-                'type': 'workflow',
-                'data': {
-                    'action': None,
-                    'review_state': state,
-                }
+                "actor": user_id,
+                "comments": "",
+                "time": datetime.datetime.now(),
+                "title": "Created",
+                "type": "workflow",
+                "data": {"action": None, "review_state": state,},
             }
         )
         workflow.register()
