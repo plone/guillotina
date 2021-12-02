2be056d4358060930e8c8645e9f7c1f28fb86ce4
Jordi Masip <jordi@masip.cat>
2021-12-02 16:14:06 +0100

    Improve error logging in futures

--- guillotina/utils/execute.py
+++ guillotina/utils/execute.py
@@ -6,6 +6,7 @@ from guillotina.interfaces import IAsyncJobPool
 from guillotina.interfaces import IQueueUtility
 from guillotina.profile import profilable
 from guillotina.transactions import get_transaction
+from guillotina.utils import notice_on_error2
 from typing import Any
 from typing import Callable
 from typing import Coroutine
@@ -204,7 +205,9 @@ def execute_futures(scope: str = "", futures=None, task=None) -> Optional[asynci
         found.append(fut)
     futures[scope] = {}
     if len(found) > 0:
-        task = asyncio.ensure_future(asyncio.gather(*found))
+        task = asyncio.ensure_future(
+            notice_on_error2("Error during after_request futures", asyncio.gather(*found))
+        )
         return task
     return None
 
